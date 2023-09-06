6db06a108f77712702fc2b967a7b313ca544bbb4
paulivingston <pau.livingston@gmail.com>
2023-09-05 22:10:27 -0300

    test

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Articulo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set;}
+        public string[] Imagen { get; set; }
+        public float Precio { get; set; }
+
+        // Marca
+
+        // Categoria
+
+
+    }
+}
