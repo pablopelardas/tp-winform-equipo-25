cad02a1a60165d88bbcc914cc80546df2fc4a23b
paulivingston <pau.livingston@gmail.com>
2023-09-06 20:30:06 -0300

    conexion a base de datos

--- DatabaseAccess/Database.cs
+++ DatabaseAccess/Database.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DatabaseAccess
 {
-    internal class Database
+    public class Database
     {
+        private SqlConnection connection;
+        private SqlCommand command;
+        public SqlDataReader reader;
+        public SqlDataReader Reader
+        {
+            get { return reader; }
+        }
+
+        public Database()
+        {
+            connection = new SqlConnection("server=.\\SQLEXPRESS;database=CATALOGO_P3_DB;integrated security=true");
+            command = new SqlCommand();
+
+        }
+
+        public void setQuery(string query)
+        {
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = query;
+        }
+
+        public void readData()
+        {
+            command.Connection = connection;
+
+            try
+            {
+                connection.Open();
+                reader = command.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+
+        }
+
+        public void CloseConnection()
+        {
+            if (reader != null) reader.Close();
+            connection.Close();
+        }
+
     }
 }
