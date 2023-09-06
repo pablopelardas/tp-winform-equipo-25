49e9d96910ab1ceebe70712543894d1249a7b918
Pablo Pelardas <pablo.pelardas@gmail.com>
2023-09-06 01:08:18 -0300

    Cambio propiedad Imagenes [] a List

--- Dominio/Articulo.cs
+++ Dominio/Articulo.cs
@@ -11,7 +11,7 @@ namespace Dominio
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set;}
-        public string[] Imagen { get; set; }
+        public List<string> Imagenes { get; set; }
         public float Precio { get; set; }
 
         // Marca
