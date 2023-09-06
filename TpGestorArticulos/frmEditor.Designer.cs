b4f2f865ea17f297402c6d31fe248f55c804a183
Pablo Pelardas <pablo.pelardas@gmail.com>
2023-09-06 01:09:41 -0300

    Agrego boton en frmPrincipal para entrar a Editor: Agrego formulario editor con precarga de datos Agrego slider de imagenes dinamico con cache para cargar las imagenes más rápido

--- TpGestorArticulos/frmEditor.Designer.cs
+++ TpGestorArticulos/frmEditor.Designer.cs
@@ -30,20 +30,21 @@
         {
             this.lblNombre = new System.Windows.Forms.Label();
             this.txtNombre = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
             this.lblPrecio = new System.Windows.Forms.Label();
             this.lblDescripcion = new System.Windows.Forms.Label();
             this.rtxtDescripcion = new System.Windows.Forms.RichTextBox();
             this.lblCategoria = new System.Windows.Forms.Label();
             this.lblMarca = new System.Windows.Forms.Label();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.ptbImagen = new System.Windows.Forms.PictureBox();
+            this.cbxCategoria = new System.Windows.Forms.ComboBox();
+            this.cbxMarca = new System.Windows.Forms.ComboBox();
+            this.pbxImagen = new System.Windows.Forms.PictureBox();
             this.btnPrevImagen = new System.Windows.Forms.Button();
             this.btnNextImagen = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtUrlImagen = new System.Windows.Forms.TextBox();
             this.lblImagen = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.ptbImagen)).BeginInit();
+            this.btnEliminarImagen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).BeginInit();
             this.SuspendLayout();
             // 
             // lblNombre
@@ -63,12 +64,12 @@
             this.txtNombre.Size = new System.Drawing.Size(206, 20);
             this.txtNombre.TabIndex = 1;
             // 
-            // textBox1
+            // txtPrecio
             // 
-            this.textBox1.Location = new System.Drawing.Point(32, 114);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(206, 20);
-            this.textBox1.TabIndex = 3;
+            this.txtPrecio.Location = new System.Drawing.Point(32, 114);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(206, 20);
+            this.txtPrecio.TabIndex = 3;
             // 
             // lblPrecio
             // 
@@ -118,30 +119,30 @@
             this.lblMarca.TabIndex = 8;
             this.lblMarca.Text = "Marca";
             // 
-            // comboBox3
+            // cbxCategoria
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(32, 181);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(237, 21);
-            this.comboBox3.TabIndex = 12;
+            this.cbxCategoria.FormattingEnabled = true;
+            this.cbxCategoria.Location = new System.Drawing.Point(32, 181);
+            this.cbxCategoria.Name = "cbxCategoria";
+            this.cbxCategoria.Size = new System.Drawing.Size(237, 21);
+            this.cbxCategoria.TabIndex = 12;
             // 
-            // comboBox1
+            // cbxMarca
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(330, 181);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(237, 21);
-            this.comboBox1.TabIndex = 13;
+            this.cbxMarca.FormattingEnabled = true;
+            this.cbxMarca.Location = new System.Drawing.Point(330, 181);
+            this.cbxMarca.Name = "cbxMarca";
+            this.cbxMarca.Size = new System.Drawing.Size(237, 21);
+            this.cbxMarca.TabIndex = 13;
             // 
-            // ptbImagen
+            // pbxImagen
             // 
-            this.ptbImagen.Location = new System.Drawing.Point(142, 233);
-            this.ptbImagen.Name = "ptbImagen";
-            this.ptbImagen.Size = new System.Drawing.Size(315, 187);
-            this.ptbImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.ptbImagen.TabIndex = 14;
-            this.ptbImagen.TabStop = false;
+            this.pbxImagen.Location = new System.Drawing.Point(142, 233);
+            this.pbxImagen.Name = "pbxImagen";
+            this.pbxImagen.Size = new System.Drawing.Size(315, 187);
+            this.pbxImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxImagen.TabIndex = 14;
+            this.pbxImagen.TabStop = false;
             // 
             // btnPrevImagen
             // 
@@ -151,6 +152,7 @@
             this.btnPrevImagen.TabIndex = 15;
             this.btnPrevImagen.Text = "←";
             this.btnPrevImagen.UseVisualStyleBackColor = true;
+            this.btnPrevImagen.Click += new System.EventHandler(this.btnPrevImagen_Click);
             // 
             // btnNextImagen
             // 
@@ -160,13 +162,15 @@
             this.btnNextImagen.TabIndex = 16;
             this.btnNextImagen.Text = "→";
             this.btnNextImagen.UseVisualStyleBackColor = true;
+            this.btnNextImagen.Click += new System.EventHandler(this.btnNextImagen_Click);
             // 
-            // textBox2
+            // txtUrlImagen
             // 
-            this.textBox2.Location = new System.Drawing.Point(142, 457);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(315, 20);
-            this.textBox2.TabIndex = 18;
+            this.txtUrlImagen.Location = new System.Drawing.Point(142, 457);
+            this.txtUrlImagen.Name = "txtUrlImagen";
+            this.txtUrlImagen.Size = new System.Drawing.Size(315, 20);
+            this.txtUrlImagen.TabIndex = 18;
+            this.txtUrlImagen.TextChanged += new System.EventHandler(this.txtUrlImagen_TextChanged);
             // 
             // lblImagen
             // 
@@ -178,30 +182,41 @@
             this.lblImagen.TabIndex = 17;
             this.lblImagen.Text = "URL";
             // 
+            // btnEliminarImagen
+            // 
+            this.btnEliminarImagen.Location = new System.Drawing.Point(434, 233);
+            this.btnEliminarImagen.Name = "btnEliminarImagen";
+            this.btnEliminarImagen.Size = new System.Drawing.Size(22, 23);
+            this.btnEliminarImagen.TabIndex = 19;
+            this.btnEliminarImagen.Text = "✕";
+            this.btnEliminarImagen.UseVisualStyleBackColor = true;
+            this.btnEliminarImagen.Click += new System.EventHandler(this.btnEliminarImagen_Click);
+            // 
             // frmEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(598, 504);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnEliminarImagen);
+            this.Controls.Add(this.txtUrlImagen);
             this.Controls.Add(this.lblImagen);
             this.Controls.Add(this.btnNextImagen);
             this.Controls.Add(this.btnPrevImagen);
-            this.Controls.Add(this.ptbImagen);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.pbxImagen);
+            this.Controls.Add(this.cbxMarca);
+            this.Controls.Add(this.cbxCategoria);
             this.Controls.Add(this.lblMarca);
             this.Controls.Add(this.lblCategoria);
             this.Controls.Add(this.rtxtDescripcion);
             this.Controls.Add(this.lblDescripcion);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPrecio);
             this.Controls.Add(this.lblPrecio);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.lblNombre);
             this.Name = "frmEditor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Editor";
-            ((System.ComponentModel.ISupportInitialize)(this.ptbImagen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -211,18 +226,19 @@
 
         private System.Windows.Forms.Label lblNombre;
         private System.Windows.Forms.TextBox txtNombre;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPrecio;
         private System.Windows.Forms.Label lblPrecio;
         private System.Windows.Forms.Label lblDescripcion;
         private System.Windows.Forms.RichTextBox rtxtDescripcion;
         private System.Windows.Forms.Label lblCategoria;
         private System.Windows.Forms.Label lblMarca;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.PictureBox ptbImagen;
+        private System.Windows.Forms.ComboBox cbxCategoria;
+        private System.Windows.Forms.ComboBox cbxMarca;
+        private System.Windows.Forms.PictureBox pbxImagen;
         private System.Windows.Forms.Button btnPrevImagen;
         private System.Windows.Forms.Button btnNextImagen;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtUrlImagen;
         private System.Windows.Forms.Label lblImagen;
+        private System.Windows.Forms.Button btnEliminarImagen;
     }
 }
--- TpGestorArticulos/frmEditor.cs
+++ TpGestorArticulos/frmEditor.cs
@@ -1,23 +1,165 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Dominio;
 
 namespace TpGestorArticulos
 {
     public partial class frmEditor : Form
     {
+
+        private List<string> Images;
+        private Dictionary<string, Image> ImageCache = new Dictionary<string, Image>();
+        private int CurrentImageIndex = 0;
         public frmEditor()
         {
             InitializeComponent();
         }
 
-        public LoadArticulo(Articulo)
+        public void LoadArticulo(Articulo articulo)
+        {
+            // Llenamos los datos del formulario con los datos del articulo
+            txtNombre.Text = articulo.Nombre.Length > 0 ? articulo.Nombre : "";
+            rtxtDescripcion.Text = articulo.Descripcion.Length > 0 ? articulo.Descripcion : "";
+            txtPrecio.Text = articulo.Precio > 0 ? articulo.Precio.ToString() : "";
+            cbxCategoria.SelectedIndex = articulo.Categoria != null ? cbxCategoria.FindStringExact(articulo.Categoria.Nombre) : -1;
+            cbxMarca.SelectedIndex = articulo.Marca != null ? cbxMarca.FindStringExact(articulo.Marca.Nombre) : -1;
+            // TODO: Verificar si es necesario hacer una copia de la lista de imagenes para evitar que se modifique el articulo original
+            Images = articulo.Imagenes;
+            // Llenamos el campo url y el picturebox con la primer imagen
+            loadImage();
+            // Deshabilitamos el boton prev si estamos en la primer imagen
+            btnPrevImagen.Enabled = CurrentImageIndex > 0;
+        }
+
+        private async Task<Image> LoadImageAsync(string url)
+        {
+            try
+            {
+                // Si la imagen ya esta en el cache, la devolvemos
+                if (ImageCache.ContainsKey(url))
+                {
+                    return ImageCache[url];
+                }
+                // Si no esta en el cache, la descargamos
+                using (HttpClient httpclient = new HttpClient())
+                {
+                    byte[] imageData = await httpclient.GetByteArrayAsync(url);
+                    using (MemoryStream ms = new MemoryStream(imageData))
+                    {
+                        // Creamos una imagen a partir de los datos descargados
+                        Image img = Image.FromStream(ms);
+                        // Agregamos la imagen al cache
+                        ImageCache[url] = img;
+                        return img;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private async void loadImage()
+        {
+            try
+            {
+                txtUrlImagen.Text = Images[CurrentImageIndex];
+                pbxImagen.Image = await LoadImageAsync(Images[CurrentImageIndex]);
+                //Si la imagen no cargó, lanzamos una excepcion
+                if (pbxImagen.Image == null) throw new Exception();
+                
+            }
+            catch (Exception)
+            {
+                //Si ocurre una excepción, mostramos la imagen de error por defecto
+                pbxImagen.Load("https://i0.wp.com/casagres.com.ar/wp-content/uploads/2022/09/placeholder.png?ssl=1");
+                txtUrlImagen.Text = "";
+            }
+        }
+
+        private void btnEliminarImagen_Click(object sender, EventArgs e)
+        {;
+            // Eliminamos la imagen actual
+            Images.RemoveAt(CurrentImageIndex);
+            // Si no quedan imagenes, deshabilitamos el boton eliminar
+            if (Images.Count == 0)
+            {
+                btnEliminarImagen.Enabled = false;
+                // Limpiamos el campo url y el picturebox
+                pbxImagen.Image = null;
+                txtUrlImagen.Text = "";
+            }
+            // Si la imagen actual es la ultima, retrocedemos una imagen
+            if (CurrentImageIndex == Images.Count)
+            {
+                CurrentImageIndex--;
+            }
+            // Si no quedan imagenes, deshabilitamos el boton prev
+            if (CurrentImageIndex == 0)
+            {
+                btnPrevImagen.Enabled = false;
+            }
+            // Cargamos la imagen actual
+
+            loadImage();
+        }
+
+        private void btnNextImagen_Click(object sender, EventArgs e)
+        {
+            // Si la imagen actual es la ultima, agregamos una imagen
+            if (CurrentImageIndex == Images.Count - 1)
+            {
+                // Si la imagen actual esta vacia, no agregamos una imagen, enviamos un mensaje de error
+                if (Images[CurrentImageIndex] == "")
+                {
+                    MessageBox.Show("Debe cargar la imagen actual para crear una nueva", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                Images.Add("");
+                btnEliminarImagen.Enabled = true;
+            }
+            // Avanzamos una imagen
+            CurrentImageIndex++;
+            // Habilitamos el boton prev
+            btnPrevImagen.Enabled = true;
+            loadImage();
+        }
+
+        private void btnPrevImagen_Click(object sender, EventArgs e)
+        {
+            // Si la imagen actual no es la primera, retrocedemos una imagen
+            if (CurrentImageIndex > 0)
+            {
+                CurrentImageIndex--;
+                // Si la imagen actual es la primera, deshabilitamos el boton prev
+                if (CurrentImageIndex == 0)
+                {
+                    btnPrevImagen.Enabled = false;
+                }
+                // Cargamos la imagen actual
+                loadImage();
+            }
+        }
+
+        private void txtUrlImagen_TextChanged(object sender, EventArgs e)
+        {
+            // Actualizamos la url de la imagen actual, si no hay imagenes, no hacemos nada
+            try
+            {
+                Images[CurrentImageIndex] = txtUrlImagen.Text;
+            }
+            catch (Exception)
+            {
+                return;   
+            }
+            
+        }
     }
 
 }
