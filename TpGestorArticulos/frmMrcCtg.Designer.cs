8bb22b907bc06beac797d01ad998a740cf15733e
paulivingston <pau.livingston@gmail.com>
2023-09-16 12:17:06 -0300

    fijar max y min de ventanas ctg/mrc

--- TpGestorArticulos/frmMrcCtg.Designer.cs
+++ TpGestorArticulos/frmMrcCtg.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.btnCancelar = new System.Windows.Forms.Button();
-            this.lblTitulo = new System.Windows.Forms.Label();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.btnEditar = new System.Windows.Forms.Button();
@@ -47,7 +46,7 @@
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(239, 441);
+            this.btnCancelar.Location = new System.Drawing.Point(241, 399);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(75, 32);
             this.btnCancelar.TabIndex = 6;
@@ -55,19 +54,10 @@
             this.btnCancelar.UseVisualStyleBackColor = true;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
-            // lblTitulo
-            // 
-            this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Location = new System.Drawing.Point(32, 28);
-            this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(45, 16);
-            this.lblTitulo.TabIndex = 0;
-            this.lblTitulo.Text = "Admin";
-            // 
             // btnAgregar
             // 
             this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAgregar.Location = new System.Drawing.Point(66, 373);
+            this.btnAgregar.Location = new System.Drawing.Point(68, 331);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(43, 35);
             this.btnAgregar.TabIndex = 8;
@@ -78,7 +68,7 @@
             // btnEliminar
             // 
             this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.Location = new System.Drawing.Point(135, 373);
+            this.btnEliminar.Location = new System.Drawing.Point(137, 331);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(43, 35);
             this.btnEliminar.TabIndex = 9;
@@ -89,7 +79,7 @@
             // btnEditar
             // 
             this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditar.Location = new System.Drawing.Point(203, 373);
+            this.btnEditar.Location = new System.Drawing.Point(205, 331);
             this.btnEditar.Name = "btnEditar";
             this.btnEditar.Size = new System.Drawing.Size(43, 35);
             this.btnEditar.TabIndex = 10;
@@ -101,7 +91,7 @@
             // 
             this.tabControl.Controls.Add(this.tabCategorias);
             this.tabControl.Controls.Add(this.tabMarcas);
-            this.tabControl.Location = new System.Drawing.Point(31, 67);
+            this.tabControl.Location = new System.Drawing.Point(33, 25);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
             this.tabControl.Size = new System.Drawing.Size(248, 300);
@@ -142,7 +132,7 @@
             this.tabMarcas.Location = new System.Drawing.Point(4, 25);
             this.tabMarcas.Name = "tabMarcas";
             this.tabMarcas.Padding = new System.Windows.Forms.Padding(3);
-            this.tabMarcas.Size = new System.Drawing.Size(240, 271);
+            this.tabMarcas.Size = new System.Drawing.Size(240, 311);
             this.tabMarcas.TabIndex = 1;
             this.tabMarcas.Text = "Marcas";
             this.tabMarcas.UseVisualStyleBackColor = true;
@@ -166,14 +156,16 @@
             // 
             // frmMrcCtg
             // 
-            this.ClientSize = new System.Drawing.Size(330, 484);
+            this.ClientSize = new System.Drawing.Size(332, 453);
             this.Controls.Add(this.tabControl);
             this.Controls.Add(this.btnEditar);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.lblTitulo);
+            this.MaximumSize = new System.Drawing.Size(350, 500);
+            this.MinimumSize = new System.Drawing.Size(350, 500);
             this.Name = "frmMrcCtg";
+            this.Text = "Admin";
             this.Load += new System.EventHandler(this.frmMrcCtg_Load);
             this.tabControl.ResumeLayout(false);
             this.tabCategorias.ResumeLayout(false);
@@ -181,13 +173,11 @@
             this.tabMarcas.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvMarcas)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button btnCancelar;
-        private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.Button btnEditar;
